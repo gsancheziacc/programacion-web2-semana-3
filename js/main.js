2f757cf3a1d9c9c6958bfbc6c5b4b8dde08c8a57
gsancheziacc <99230574+gsancheziacc@users.noreply.github.com>
2022-06-26 23:54:32 -0400

    Tarea semana 3

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Perfectamente se pueden declarar como var en vez de const, pero hoy en día
+// javascript moderno permite definir constantes con const
+const numberA = 1;
+const numberB = 2;
+const numberC = 3.5;
+const numberD = 4.5;
